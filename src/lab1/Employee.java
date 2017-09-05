e0424532ab440d6b5d12c5ab98f1106824a11866
JohnDeWalt <Jackie@10.30.12.25>
2017-09-05 10:26:53 -0500

    Adjusting the constuctor

--- src/lab1/Employee.java
+++ src/lab1/Employee.java
@@ -22,6 +22,91 @@ public class Employee {
     private String cubeId;
     private Date orientationDate;
 
+    public Employee(String firstName, String lastName, String ssn, String cubeId, Date orientationDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ssn = ssn;
+        //this.metWithHr = metWithHr;
+        //this.metDeptStaff = metDeptStaff;
+        //this.reviewedDeptPolicies = reviewedDeptPolicies;
+        //this.movedIn = movedIn;
+        this.cubeId = cubeId;
+        this.orientationDate = orientationDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public boolean isMetWithHr() {
+        return metWithHr;
+    }
+
+    public boolean isMetDeptStaff() {
+        return metDeptStaff;
+    }
+
+    public boolean isReviewedDeptPolicies() {
+        return reviewedDeptPolicies;
+    }
+
+    public boolean isMovedIn() {
+        return movedIn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public Date getOrientationDate() {
+        return orientationDate;
+    }
+
+    public void setFirstName(String firstName) {
+   
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setMetWithHr(boolean metWithHr) {
+        this.metWithHr = metWithHr;
+    }
+
+    public void setMetDeptStaff(boolean metDeptStaff) {
+        this.metDeptStaff = metDeptStaff;
+    }
+
+    public void setReviewedDeptPolicies(boolean reviewedDeptPolicies) {
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+    }
+
+    public void setMovedIn(boolean movedIn) {
+        this.movedIn = movedIn;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    public void setOrientationDate(Date orientationDate) {
+        this.orientationDate = orientationDate;
+    }
+
 
 
 }
